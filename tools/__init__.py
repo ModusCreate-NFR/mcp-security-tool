a233e0ac0b16cae8e77f2a8e383b9a520a3a5b0b
Samuel Afolabi <151579415+Lordsammm@users.noreply.github.com>
2026-02-17 13:09:05 +0100

    Update __init__.py

--- tools/__init__.py
+++ tools/__init__.py
@@ -7,6 +7,11 @@ from .dns import dns_lookup, whois_lookup, crtsh_lookup
 from .web import http_headers_check, ffuf_fuzz, katana_crawl, waybackurls_fetch
 from .vuln import nuclei_scan, sqlmap_scan, sslscan_check
 from .recon import amass_enum, assetfinder_enum
+from .injection import commix_scan, smuggler_scan
+from .wordpress import wpscan_scan
+from .cloud import scoutsuite_scan
+from .discovery import alterx_generate, arjun_scan, shuffledns_scan, gowitness_screenshot, cero_scan
+from .mobile import mobsf_scan
 
 # All available tools
 ALL_TOOLS = {
@@ -34,6 +39,26 @@ ALL_TOOLS = {
     # Recon Tools
     "amass_enum": amass_enum,
     "assetfinder_enum": assetfinder_enum,
+    
+    # Injection Tools
+    "commix_scan": commix_scan,
+    "smuggler_scan": smuggler_scan,
+    
+    # WordPress Tools
+    "wpscan_scan": wpscan_scan,
+    
+    # Cloud Security Tools
+    "scoutsuite_scan": scoutsuite_scan,
+    
+    # Discovery Tools
+    "alterx_generate": alterx_generate,
+    "arjun_scan": arjun_scan,
+    "shuffledns_scan": shuffledns_scan,
+    "gowitness_screenshot": gowitness_screenshot,
+    "cero_scan": cero_scan,
+    
+    # Mobile Security Tools
+    "mobsf_scan": mobsf_scan,
 }
 
 __all__ = list(ALL_TOOLS.keys()) + ["ALL_TOOLS"]
